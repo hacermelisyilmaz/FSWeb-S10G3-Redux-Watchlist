075879b31dcf900644353a0625da0d5e884c2060
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-08-31 11:18:28 +0300

    removeFav action maker

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,3 +2,7 @@ export const PREV_MOVIE = "Önceki filme geç";
 export const NEXT_MOVIE = "Sonraki filme geç";
 export const ADD_FAV = "Favoriye ekle";
 export const REMOVE_FAV = "Favoriden çıkar";
+
+export const removeFav = (id) => {
+  return { type: REMOVE_FAV, payload: id };
+};
